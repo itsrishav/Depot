25a7a925c15e4851f794571a41439681dd1d20a5
itsrishav <with.rishav@gmail.com>
2016-01-05 17:29:48 +0530

    Added 'Add to Cart' button to add products to line_item. Also there respective price in currency using 'number_to_currency'. And there total price in a cart with there css styling views.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+	has_many :line_items
+	before_destroy :ensure_not_reference_to_any_line_item
 	validates :title, uniqueness: true, presence: true
 	validates :description, presence: true
 	validates :image_url, format:{
@@ -6,4 +8,16 @@ class Product < ActiveRecord::Base
 		message: "must be a URL for GIF, JPG or PNG image."
 	}
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	def self.latest
+		Product.order(:updated_at).last
+	end
+private
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+			return false
+		end
+	end
 end
